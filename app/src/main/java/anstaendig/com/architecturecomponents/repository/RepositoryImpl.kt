bfe4a1c389e2d7ba544034d54947407102cd257b
Jon Duran <jon.duran@zalando.fi>
2017-06-28 15:56:33 +0300

    Cleanup + improve abstraction

--- app/src/main/java/anstaendig/com/architecturecomponents/repository/RepositoryImpl.kt
+++ app/src/main/java/anstaendig/com/architecturecomponents/repository/RepositoryImpl.kt
@@ -1,13 +1,8 @@
 package anstaendig.com.architecturecomponents.repository
 
-import android.net.Uri
-import anstaendig.com.architecturecomponents.datasource.PageData
-import anstaendig.com.architecturecomponents.datasource.PersonData
 import anstaendig.com.architecturecomponents.datasource.SwapiService
-import io.reactivex.Completable
 import io.reactivex.Observable
 import io.reactivex.ObservableTransformer
-import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
@@ -16,22 +11,6 @@ class RepositoryImpl
 @Inject
 constructor(private val swapiService: SwapiService) : Repository {
 
-  override fun loadPeople(): Single<PageData<PersonData>>
-      = swapiService.loadPeople().subscribeOn(Schedulers.io())
-
-  override fun loadPeople(url: Uri): Single<PageData<PersonData>>
-      = swapiService.loadPeople(url).subscribeOn(Schedulers.io())
-
-  override fun searchPeople(name: String): Single<PageData<PersonData>>
-      = swapiService.searchPeople(name).subscribeOn(Schedulers.io())
-
-  override fun loadPerson(id: String): Single<PersonData>
-      = swapiService.loadPerson(id).subscribeOn(Schedulers.io())
-
-  override fun addPersonToFavorites(personData: PersonData): Completable {
-    TODO()
-  }
-
   private val loadPerson: ObservableTransformer<Action.LoadPerson, Result> = ObservableTransformer { actions ->
     actions
         .flatMap { (id) ->
@@ -39,27 +18,32 @@ constructor(private val swapiService: SwapiService) : Repository {
               .loadPerson(id)
               .subscribeOn(Schedulers.io())
               .toObservable()
+              .map<Result> { personData -> Result.Success(personData) }
+              .startWith(Result.InProgress)
+              .onErrorReturn { e -> Result.Failure(e.message!!) }
         }
-        .map<Result> { personData -> Result.Success(personData) }
-        .onErrorReturn { e -> Result.Failure(e.message!!) }
         .observeOn(AndroidSchedulers.mainThread())
-        .startWith(Result.InProgress)
   }
 
-  private val actionTwoResult: ObservableTransformer<Action.Two, Result> = ObservableTransformer { actions ->
+  private val loadLuke: ObservableTransformer<Action.LoadLuke, Result> = ObservableTransformer { actions ->
     actions
-        .flatMap { _ -> loadPerson("1").toObservable() }
-        .map<Result> { personData -> Result.Success(personData) }
-        .onErrorReturn { e -> Result.Failure(e.message!!) }
+        .flatMap { _ ->
+          swapiService
+              .loadPerson("1")
+              .subscribeOn(Schedulers.io())
+              .toObservable()
+              .map<Result> { personData -> Result.Success(personData) }
+              .onErrorReturn { e -> Result.Failure(e.message!!) }
+              .startWith(Result.InProgress)
+        }
         .observeOn(AndroidSchedulers.mainThread())
-        .startWith(Result.InProgress)
   }
 
   override val results: ObservableTransformer<Action, Result> = ObservableTransformer { events ->
     events.publish { shared ->
       Observable.merge(
           shared.ofType(Action.LoadPerson::class.java).compose(loadPerson),
-          shared.ofType(Action.Two::class.java).compose(actionTwoResult)
+          shared.ofType(Action.LoadLuke::class.java).compose(loadLuke)
       )
     }
   }
--- app/src/main/java/anstaendig/com/architecturecomponents/viewmodel/MainActivityViewModel.kt
+++ app/src/main/java/anstaendig/com/architecturecomponents/viewmodel/MainActivityViewModel.kt
@@ -3,63 +3,51 @@ package anstaendig.com.architecturecomponents.viewmodel
 import anstaendig.com.architecturecomponents.repository.Action
 import anstaendig.com.architecturecomponents.repository.Repository
 import anstaendig.com.architecturecomponents.repository.Result
+import anstaendig.com.architecturecomponents.ui.MainActivityUiEvent
 import anstaendig.com.architecturecomponents.ui.MainActivityViewState
-import anstaendig.com.architecturecomponents.ui.UiEvent
+import anstaendig.com.architecturecomponents.ui.event.UiEvent
 import anstaendig.com.architecturecomponents.viewmodel.base.BaseViewModel
 import io.reactivex.Observable
 import io.reactivex.ObservableTransformer
-import io.reactivex.exceptions.OnErrorNotImplementedException
-import io.reactivex.subjects.BehaviorSubject
-import io.reactivex.subjects.Subject
 import javax.inject.Inject
 
 class MainActivityViewModel
 @Inject
 constructor(repository: Repository) : BaseViewModel<MainActivityViewState>() {
 
-  var initialState: MainActivityViewState = MainActivityViewState.Idle
-
-  val onTextChange: ObservableTransformer<UiEvent.OnTextChange, Action.LoadPerson> = ObservableTransformer { events ->
-    events.map { (text) -> Action.LoadPerson(text) }
+  private val onTextChange: ObservableTransformer<MainActivityUiEvent.OnTextChange, Action.LoadPerson> = ObservableTransformer { events ->
+    events.distinctUntilChanged()
+        .map { (text) -> Action.LoadPerson(text) }
   }
 
-  val onMessageClick: ObservableTransformer<UiEvent.OnMessageClick, Action.Two> = ObservableTransformer { events ->
-    events.map { Action.Two }
+  private val onMessageClick: ObservableTransformer<MainActivityUiEvent.OnMessageClick, Action.LoadLuke> = ObservableTransformer { events ->
+    events.map { Action.LoadLuke }
   }
 
-  // TODO: Use RxRelay instead of subjects
-  val events: Subject<UiEvent> = BehaviorSubject.create<UiEvent>()
-
-  val actions: ObservableTransformer<UiEvent, Action> = ObservableTransformer { events ->
+  private val actions: ObservableTransformer<UiEvent, Action> = ObservableTransformer { events ->
     events.publish<Action> { shared ->
       Observable.merge(
-          shared.ofType(UiEvent.OnTextChange::class.java).compose(onTextChange),
-          shared.ofType(UiEvent.OnMessageClick::class.java).compose(onMessageClick)
+          shared.ofType(MainActivityUiEvent.OnTextChange::class.java).compose(onTextChange),
+          shared.ofType(MainActivityUiEvent.OnMessageClick::class.java).compose(onMessageClick)
       )
     }
   }
 
-  val state: Observable<MainActivityViewState> =
-      events
-          .compose(actions)
-          .compose(repository.results)
-          .scan<MainActivityViewState>(initialState, { state, result ->
-            initialState = state
-            when (result) {
-              is Result.Success -> MainActivityViewState.Success(result.data)
-              is Result.Failure -> MainActivityViewState.Failure(result.e)
-              is Result.InProgress -> MainActivityViewState.InProgress
-            }
-          })
-
   init {
-    disposables.addAll(
-        state.subscribe({ state ->
-          viewState.value = state
-        }, { throwable ->
-          throw OnErrorNotImplementedException(throwable)
-        })
+    // TODO Try to find a better Kotlin-ish way of doing this...
+    super.init(
+        events
+            .compose(actions)
+            .compose(repository.results)
+            .scan<MainActivityViewState>(MainActivityViewState.Idle, { state, result ->
+              when (result) {
+                is Result.Success -> MainActivityViewState.Success(result.data)
+                is Result.Failure -> MainActivityViewState.Failure(result.e)
+                is Result.InProgress -> MainActivityViewState.InProgress
+              }
+            })
     )
   }
+
 }
 
